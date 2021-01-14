420725305a56e096ec8970cd7992dc24795e1547
ROCIO <rociogonuri@gmail.com>
2021-01-14 13:11:46 +0100

    código sin javadoc 53364034

---
+++ src/analistaed_53364034d/AnalistaED_53364034D.java
@@ -0,0 +1,54 @@
+package analistaed_53364034d;
+
+/**
+ * Ejercicio práctico examen EVAL 1 ED
+ *
+ * @author ROCIO GONZALEZ DNI 53364034D
+ * @version 1.0
+ */
+public class AnalistaED_53364034D {
+
+   static final double FACTOR_HORA_EXTRA = 1.2;
+   private String nombreCompleto, dni;
+   private double salarioBasePorHora = 10.56;
+
+   /**
+    * Constructor con tres parámetros
+    *
+    * @param nombreCompleto nombre completo en mayúsculas
+    * @param dni documento nacional de identidad con letra y sin espacios
+    */
+   public AnalistaED_53364034D(String nombreCompleto, String dni) {
+      this.nombreCompleto = nombreCompleto;
+      this.dni = dni;
+   }
+
+   /**
+    * Calcula el salario bruto mensual en función del salario base por hora y
+    * las horas extras El precio por hora de la hora extra se determina con la
+    * constante FACTOR_HORA_EXTRA
+    *
+    * @param horasExtra número de horas extra dedicadas redondeando a la baja
+    * (entero)
+    *
+    * @return devuelve el salario bruto mensual en euros con dos decimales
+    */
+   public double getSalarioEsteMes(int horasExtra) {
+      double cantidadExtra, salarioFinal;
+      cantidadExtra = horasExtra * this.salarioBasePorHora * FACTOR_HORA_EXTRA;
+      salarioFinal = this.salarioBasePorHora * 40 * 4 + cantidadExtra;
+      salarioFinal = (double) Math.round(salarioFinal * 100d) / 100d;
+      return salarioFinal;
+   }
+
+   // Este método no lo comentaremos con JavaDoc
+   public static void main(String[] args) {
+      // !!!! MODIFICA TU NOMBRE Y TU DNI EN LA SIGUIENTE LÍNEA
+      AnalistaED_53364034D emp = new AnalistaED_53364034D("ROCÍO GONZÁLEZ", "53364034d");
+      int horasExtra = 2;
+      System.out.println("El salario de " + emp.nombreCompleto
+         + " este mes será de " + emp.getSalarioEsteMes(horasExtra)
+         + " euros, al hacer " + horasExtra + " horas extra.");
+   }
+
+}
